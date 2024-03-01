958160a1f7cc7cb1e66d033abacad2c992e114ba
Ridan Hawy <113379842+Hawy08@users.noreply.github.com>
2024-03-01 18:54:21 +0300

    Animation to landing page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,25 +12,28 @@ function Hero() {
               <div className="mb-4">
                 <Header />
               </div>
-              <p className="text-3xl font-bold py-12 w-2/3">
+              <p className="text-3xl font-bold py-12 w-2/3 at-item">
                 We are Naturing Answers to your Agricultural Questions
               </p>
-              <p className="py-8 tracking-wide w-2/3">
+              <p className="py-8 tracking-wide w-2/3 at-item">
                 Giving farmers insights through well researched articles,
                 answering their questions real-time and bridging the knowledge
                 gap.{" "}
               </p>
-              <button className="bg-green-500 shadow-xl p-2 rounded-md my-8 tracking-wide text-white hover:scale-105 transition-all duration-500 ease-linear">
+              <button className="bg-green-500 shadow-xl p-2 rounded-md my-8 tracking-wide text-white hover:scale-105 transition-all duration-500 ease-linear at-item">
                 Learn Now
               </button>
             </div>
           </div>
           <div className=" w-2/3">
-            <div className="bg h-full c-path flex flex-col-reverse">
-                <div className="h-2/3 w-full ">
-                <img src={man} alt="man reading" className="h-full w-full object-cover"/>
-                </div>
-
+            <div className="bg h-full c-path flex flex-col-reverse at-item">
+              <div className="h-2/3 w-full ">
+                <img
+                  src={man}
+                  alt="man reading"
+                  className="h-full w-full object-cover"
+                />
+              </div>
             </div>
             {/* <div className="w-full  bg-red-200 ">
                 <p>A farmer</p>
